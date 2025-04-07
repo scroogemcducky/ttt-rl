b8657f8f722cfa70ec3017a73a153f9b79b0d3d3
McDuck <rami.lejmi@helsinki.fi>
2025-04-07 12:02:12 +0300

    Fixing typo

--- server/main.py
+++ server/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     # allow_origins=["https://rl-client.pages.dev"],
-    allow_origins=["*"]
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
