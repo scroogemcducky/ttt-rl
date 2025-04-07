b238f9619f77f4d49ad2606460a51db693a1d08a
McDuck <rami.lejmi@helsinki.fi>
2025-04-07 11:50:41 +0300

    Allow other origins

--- server/main.py
+++ server/main.py
@@ -7,7 +7,8 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://rl-client.pages.dev"],  # For development only - restrict this in production
+    # allow_origins=["https://rl-client.pages.dev"],
+    allow_origins=["*"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
